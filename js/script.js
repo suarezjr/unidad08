e3ca3afb7e4be5598ae323cd296523b2167a9624
suarezjr <suarezj@frcu.utn.edu.ar>
2024-10-28 17:45:29 -0300

    Ajustes de Responsibilidad para entrega final. Productos y Contacto

--- js/script.js
+++ js/script.js
@@ -14,3 +14,18 @@ for (let i = 0; i < 5; i++) {
 //  alert(nombre);
 
 
+function calcularDistancia(x1, y1, x2, y2) {
+  let dx = x2 - x1;
+  let dy = y2 - y1;
+  return Math.sqrt(dx * dx + dy * dy);
+}
+
+let distancia = calcularDistancia(0, 0, 10, 10);
+console.log("La distancia es: "); // Muestra la distancia entre los puntos
+console.log(distancia); // Muestra la distancia entre los puntos
+
+
+function introducir(nombre, edad) {
+  console.log(`Hola, mi nombre es ${nombre} y tengo ${edad} años.`);
+}
+introducir("Ana", 25);
